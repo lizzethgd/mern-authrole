bb2fa12d1aef51ec0589d607afbccff0a49a6c06
lizzethgd <lizzethgd@gmail.com>
2020-09-03 23:59:39 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Auth from './authPage/Auth.js'
+import './authPage/Auth.css'
+
+function App() {
+  return (
+    <div >
+      <Auth />
+    </div>
+  );
+}
+
+export default App;
